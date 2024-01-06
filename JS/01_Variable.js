c283a971b188adbc2b7f33d2138482b6fe80a40e
ashirbad dash <ashirbadash2000@gmail.com>
2024-01-06 03:45:19 +0000

    "2"

---
+++ JS/01_Variable.js
@@ -0,0 +1,41 @@
+console.log("Hello world");
+const accountId = 1;
+// accountId=2 const is constant value and cannot be changed
+console.log(accountId);
+
+let accountEmail = "ashirbadash2000@gmail.com"; //LET IS BLOCK SCOPE 
+
+
+accountEmail = "ash@gmail.com";
+
+{
+    let accountEmail = "ashirbadash2000@gmail.com";
+  var accountPassword = "12345";
+  console.log(accountPassword);
+}
+var accountPassword = "123";
+console.log(accountPassword);
+
+var accountCity = "m";
+
+accountCity = "Bengaluru";
+
+let accountState = "karnataka";
+
+/*
+Prefered not to use var because of issue in block scope and functional scope
+
+
+*/
+// accountId=2
+accountCity = "bangalore";
+
+console.table([
+  accountId,
+  accountEmail,
+  accountPassword,
+  accountCity,
+  accountState,
+]);
+
+console.log(accountId);
---
+++ JS/02_Datatypes.js
@@ -0,0 +1,4 @@
+"use strict";//treat all js code as newer ajs
+
+alert("Hello")
+
