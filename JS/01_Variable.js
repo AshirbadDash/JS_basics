6ecb90b563fb1079e382261a3c74761391bd4664
ashirbad dash <ashirbadash2000@gmail.com>
2024-01-06 03:57:09 +0000

    3

--- JS/01_Variable.js
+++ JS/01_Variable.js
@@ -3,13 +3,12 @@ const accountId = 1;
 // accountId=2 const is constant value and cannot be changed
 console.log(accountId);
 
-let accountEmail = "ashirbadash2000@gmail.com"; //LET IS BLOCK SCOPE 
-
+let accountEmail = "ashirbadash2000@gmail.com"; //LET IS BLOCK SCOPE
 
 accountEmail = "ash@gmail.com";
 
 {
-    let accountEmail = "ashirbadash2000@gmail.com";
+  let accountEmail = "ashirbadash2000@gmail.com";
   var accountPassword = "12345";
   console.log(accountPassword);
 }
--- JS/02_Datatypes.js
+++ JS/02_Datatypes.js
@@ -1,4 +1,25 @@
-"use strict";//treat all js code as newer ajs
+"use strict"; //treat all js code as newer ajs
 
-alert("Hello")
+alert(3 + 2); //we are using node js not browser
 
+console.log(3 + 
+    3);
+//readability should high
+console.log("Ashirbad");
+
+let name = "Ash";
+let age = 18;
+let isLogin = true;
+
+//primitive
+
+//number ==>2 to power 53
+//bigint ==>big numbers
+//string ==>" " /' ' prefer " "
+// Boolean ==>true /false
+// null ==>standalone value(empty)
+//undefined ==>not initialized
+//symbol ==>uniqueness
+
+//object
+console.log("typeOf");
